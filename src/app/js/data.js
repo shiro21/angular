3fa40c94c2dbefc90ad273c4e0560baf397291fc
노준혁 <junhyeok403@naver.com>
2022-07-13 18:06:40 +0900

    Fix : owltastic 완료

--- src/app/js/data.js
+++ src/app/js/data.js
@@ -5,14 +5,14 @@ const projects = [
     move: 'https://shiro21.tistory.com/'
   },
   {
-    img: '../assets/icon/tistory.svg',
-    title: '티스토리',
-    move: 'https://shiro21.tistory.com/'
+    img: '../assets/icon/github.svg',
+    title: '깃허브',
+    move: 'https://github.com/shiro2'
   },
   {
-    img: '../assets/icon/tistory.svg',
-    title: '티스토리',
-    move: 'https://shiro21.tistory.com/'
+    img: '../assets/icon/facebook.svg',
+    title: '페이스북',
+    move: 'https://www.facebook.com/pacapen1122'
   }
 ];
 
--- src/app/pages/join/join.component.ts
+++ src/app/pages/join/join.component.ts
@@ -7,9 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinComponent implements OnInit {
 
+  id: String = '';
+  password: String = '';
+  passwordConfirm: String = '';
+  nick: String = '';
+  year: String = '';
+  gender: String = '성별';
+  month: String = '월';
+  day: String = '';
+  email: String = '';
+  phone: String = '';
+  months: any = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // ID Check
+  idCheckConfirm: boolean = false;
+  passwordCheckConfirm: boolean = false;
+  passwordConfirmCheckConfirm: boolean = false;
+  check(list: any) {
+    switch(list) {
+      case 'id' :
+        if(this.id.length <= 3) {
+          this.idCheckConfirm = false;
+        } else if(this.id.length > 3) {
+          this.idCheckConfirm = true;
+        }
+      break;
+
+      case 'password' :
+        if(this.password.length <= 7) {
+          this.passwordCheckConfirm = false;
+        } else if(this.password.length > 7) {
+          this.passwordCheckConfirm = true;
+        }
+      break;
+
+      case 'passwordConfirm' :
+        if(this.passwordConfirm !== this.password) {
+          this.passwordConfirmCheckConfirm = false;
+        } else if(this.passwordConfirm === this.password) {
+          this.passwordConfirmCheckConfirm = true;
+        }
+      break;
+    }
+  }
+
+  join() {
+    console.log(Number(this.year));
+    console.log(Number(this.month));
+    console.log(Number(this.day));
+    if(this.id.length === 0 || this.idCheckConfirm === false) {
+      alert('아이디를 입력해주세요.');
+    } else if(this.password !== this.passwordConfirm || this.passwordConfirmCheckConfirm === false) {
+      alert('패스워드를 확인해주세요.');
+    } else if(this.nick.length === 0) {
+      alert('이름을 입력해주세요.');
+    } else if(Number(this.year) === NaN || this.year.length !== 4) {
+      alert('년도를 확인해주세요.');
+    } else if(Number(this.month) === NaN) {
+      alert('월을 확인해주세요.');
+    } else if(Number(this.day) === NaN || this.day === '0') {
+      alert('일을 확인해주세요.');
+    } else { alert('양호') };
   }
 
 }
